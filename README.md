2c287c9e936b488cd814aaaf0253c237c1dbafd7
shane <shaneawhitlock@gmail.com>
2017-03-21 20:46:15 -0600

    did the exercises and answered questions in readme.md

--- README.md
+++ README.md
@@ -3,17 +3,22 @@
 ## Instructions
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
-		
-	* Callback Functions
-	* Closure
-	* arguments array
-	* recursion
-	* prototype
-	* constructors
 
+	* Callback Functions
+	Callback functions are functions that are created outside of another function, but called within that other function.
+	* Closure
+	Closures are functions that use variables that are defined in the parent of another function by using that function as a callback. effectively getting into the scope of a previously defined function and getting the variables with the scope that they have in that previous function.
+	* arguments array
+	the arguments array is an object that is on any function. that would include any parameters that are passed into a function.
+	* recursion
+	recursion is using a function to call back upon itself.
+	* prototype
+	Prototype is a method that you can put on a constructor or a class that allows you to use that method on any object of that same class. this makes it so you don't have to create the method on every single object.
+	* constructors
+	constructors are methods that define a particular type of object that allows you to easily create multiple objects and give values to previously defined attributes of that object.
 
 2. Fork and clone this repo.  When you need to commit use the following commands.
-		
+
 	* git status
 	* git add --all
 	* git status
--- exercises.js
+++ exercises.js
@@ -6,27 +6,48 @@ function multiplyArguments() {
 	//use the arguments keyword to multiply all of the arguments together and return the product
 	//if no arguments are passed in return 0
 	//if one argument is passed in just return it
+	var j = 1;
+	if (arguments.length < 1) {
+		return 0;
+	}
+	for (var i = 0; i < arguments.length; i++) {
+		j = arguments[i] * j;
+	} return j;
 }
 
 function invokeCallback(cb) {
 	//invoke cb
+	cb();
 }
 
 function sumArray(numbers, cb) {
 	//sum up all of the integers in the numbers array
 	//pass the result to cb
 	//no return is necessary
+	var sum = 0;
+	for (var i = 0; i < numbers.length; i++){
+		sum += numbers[i];
+	}
+	cb(sum);
 }
+
 
 function forEach(arr, cb) {
 	//iterate over arr and pass its values to cb one by one
 	//hint: you will be invoking cb multiple times (once for each value in the array)
-}
+	arr.forEach(function (blah){
+		cb(blah);
+	});
+	}
 
 function map(arr, cb) {
 	//create a new array
 	//iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
 	//the new array should be the same length as the array argument
+	var newarr = arr.map( function(blah) {
+		return cb(blah);
+	});
+	return newarr;
 }
 
 function getUserConstructor() {
@@ -36,11 +57,25 @@ function getUserConstructor() {
 	//the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
 	//{{name}} should be the name set on each instance
 	//return the constructor
+	function User(options) {
+		this.username = options.username;
+		this.name = options.name;
+		this.email = options.email;
+		this.password = options.password;
+	}
+	User.prototype.SayHi = function() {
+		return 'Hello my name is {{name}}';
+	};
+	return User;
 }
 
 function addPrototypeMethod(Constructor) {
 	//add a method to the constructor's prototype
 	//the method should be called 'sayHi' and should return the string 'Hello World!'
+	Constructor.prototype.sayHi = function() {
+		return 'Hello World!';
+	};
+
 }
 
 function addReverseString() {
@@ -48,6 +83,13 @@ function addReverseString() {
 	//name this method reverse
 	//hint:
 	//you will need to use 'this' inside of reverse
+	var newstring = '';
+	String.prototype.reverse = function() {
+		for (var i = this.length -1; i>= 0; i--) {
+			newstring += this[i];
+		}
+		return newstring;
+	};
 }
 
 function nFactorial(n) {
@@ -55,6 +97,11 @@ function nFactorial(n) {
 	//solve this recursively
 	//example:
 	//the factorial of 3 is 6 (3 * 2 * 1)
+	if (n === 0) {
+		return 1;
+	} else {
+		return n * nFactorial(n-1);
+	}
 }
 
 function cacheFunction(cb) {
